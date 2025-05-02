b824008a34b30dc0aec1c926cf0f89b37b94180b
yungangwu <solariswu@gmail.com>
2025-05-02 18:07:36 +1000

    change mk_beta to mk_pre-release

--- mk_pre-release.sh
+++ mk_pre-release.sh
@@ -5,11 +5,11 @@ set -e
 AMFA_FOLD=$(basename "$(pwd)")
 
 check_folder() {
-    if [ -d "../${AMFA_FOLD}_beta" ]; then
-        echo "../${AMFA_FOLD}_beta already exists"
+    if [ -d "../${AMFA_FOLD}_pre-release" ]; then
+        echo "../${AMFA_FOLD}_pre-release already exists"
         read -p "Do you want to override it? (y/n) " response
         if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-            rm -rf "../${AMFA_FOLD}_beta"
+            rm -rf "../${AMFA_FOLD}_pre-release"
         else
             exit 1
         fi
@@ -17,7 +17,7 @@ check_folder() {
 }
 
 build_project() {
-    echo 'Making beta... wait'
+    echo 'Making pre-release ... wait'
     npm install --legacy-peer-deps
     npm run build
     npm run lambda-build
@@ -26,12 +26,12 @@ build_project() {
 
 copy_project() {
     cd ..
-    cp -r "$AMFA_FOLD" "${AMFA_FOLD}_beta" >/dev/null 2>&1
-    cd "${AMFA_FOLD}_beta"
+    cp -r "$AMFA_FOLD" "${AMFA_FOLD}_pre-release" >/dev/null 2>&1
+    cd "${AMFA_FOLD}_pre-release"
 }
 
 clean_up() {
-    rm -rf cdk.out .git node_modules src public mk_release.sh mk_beta.sh config_bak.sh
+    rm -rf cdk.out .git node_modules src public mk_release.sh mk_pre-release.sh config_bak.sh
     rm -rf cdk/lambda/serviceproviderslist/*.mjs cdk/lambda/serviceproviderslist/dist/*.map
     rm -rf cdk/lambda/samlslist/*.mjs cdk/lambda/samlslist/dist/*.map
     rm -rf cdk/lambda/smtpconfig/*.mjs cdk/lambda/smtpconfig/dist/*.map
@@ -45,17 +45,17 @@ clean_up() {
 initialize_git() {
     git init >/dev/null 2>&1
     git add . >/dev/null 2>&1
-    git commit -m "beta version" >/dev/null 2>&1
+    git commit -m "pre rlease version" >/dev/null 2>&1
 }
 
 main() {
     check_folder
-    git checkout beta >/dev/null 2>&1
+    git checkout pre-release >/dev/null 2>&1
     build_project
     copy_project
     clean_up
     initialize_git
-    echo "Beta made in folder ../${AMFA_FOLD}_beta"
+    echo "Pre-release version made in folder ../${AMFA_FOLD}_pre-release"
 }
 
 main "$@"
