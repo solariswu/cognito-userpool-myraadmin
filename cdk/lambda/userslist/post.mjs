3adb304b9852a7763e0543205f4889e018dc646c
yungangwu <solariswu@gmail.com>
2025-05-05 10:51:59 +1000

    correct func name

--- cdk/lambda/userslist/post.mjs
+++ cdk/lambda/userslist/post.mjs
@@ -25,9 +25,14 @@ function getRandomNumber() {
     return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
 }
 
+function getRandomSymbol() {
+    const symbols = '!@#$%^&*(){}[]=<>/,.'
+    return symbols[Math.floor(Math.random() * symbols.length)]
+}
+
 const  randomFunc = {
     upper: getRandomUpper,
-    lower: getRondomLower,
+    lower: getRandomLower,
     number: getRandomNumber,
     symbol: getRandomSymbol
 };
